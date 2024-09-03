a4e7ebe1cb9ba51a8ca16377aa3f65ce8e962e22
klen25 <noval.kurniawan93@gmail.com>
2024-09-03 22:52:43 +0700

    Add files via upload

---
+++ WriteMultipleFields/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Pasmadala22"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "Pasmadala22"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 811953			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "K8MO36HXY7CU0LZI"   // replace XYZ with your channel write API Key
